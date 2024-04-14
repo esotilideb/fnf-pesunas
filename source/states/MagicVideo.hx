210e03e5a1b7e28e611edc78e84f0d5ed0a157ea
FacunMax <148605202+FacunMax@users.noreply.github.com>
2024-04-13 23:33:10 -0300

    Add files via upload

---
+++ source/states/MagicVideo.hx
@@ -0,0 +1,80 @@
+package states;
+
+import flixel.FlxG;
+import flixel.FlxState;
+import flixel.util.FlxTimer;
+import flixel.util.FlxSpriteUtil;
+
+import cutscenes.CutsceneHandler;
+
+#if VIDEOS_ALLOWED
+#if (hxCodec >= "3.0.0") import hxcodec.flixel.FlxVideo as VideoHandler;
+#elseif (hxCodec >= "2.6.1") import hxcodec.VideoHandler as VideoHandler;
+#elseif (hxCodec == "2.6.0") import VideoHandler;
+#else import vlc.MP4Handler as VideoHandler; #end
+#end
+
+class MagicVideo extends FlxState
+{
+    override public function create()
+    {
+        startVideo('rap_insano');
+        super.create();
+        FlxG.sound.playMusic(Paths.music('freakyMenu'), 0);
+        FlxG.sound.music.fadeIn(0.1, 0, 0);
+    }
+
+    override public function update(elapsed){
+        super.update(elapsed);
+        if (FlxG.keys.justPressed.SPACE) {
+            FlxG.switchState(new MainMenuState());
+        }    
+    }
+
+    public function startVideo(name:String)
+        {
+            #if VIDEOS_ALLOWED
+    
+            var filepath:String = Paths.video(name);
+            #if sys
+            if(!FileSystem.exists(filepath))
+            #else
+            if(!OpenFlAssets.exists(filepath))
+            #end
+            {
+                FlxG.log.warn('Couldnt find video file: ' + name);
+                startAndEnd();
+                return;
+            }
+    
+            var video:VideoHandler = new VideoHandler();
+                #if (hxCodec >= "3.0.0")
+                // Recent versions
+                video.play(filepath);
+                video.onEndReached.add(function()
+                {
+                    video.dispose();
+                    startAndEnd();
+                    return;
+                }, true);
+                #else
+                // Older versions
+                video.playVideo(filepath);
+                video.finishCallback = function()
+                {
+                    startAndEnd();
+                    return;
+                }
+                #end
+            #else
+            FlxG.log.warn('Platform not supported!');
+            startAndEnd();
+            return;
+            #end
+        }
+    
+        function startAndEnd()
+        {
+        }
+}
+
